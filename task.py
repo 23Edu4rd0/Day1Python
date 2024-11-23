920c6594a5b53b6b032d8dd4cb1be9f63c2d16ff
Eduardo <23eduardoviana@gmail.com>
2024-11-23 15:14:27 -0300

    Day1 Pyhton
    First project

---
+++ task.py
@@ -0,0 +1,6 @@
+print("Welcome to the band name Generator" )
+cityName = input("What's the name of the city you grown up?\n")
+
+petName = input("What's your pet name?\n")
+
+print("Your band name could be " + cityName + " " + petName)
